339cb3eb7170efbd60d6dba79caa0af0dedabd55
Mikubo-1211 <mikubo-daichi1985@g.ecc.u-tokyo.ac.jp>
2023-06-10 00:54:27 +0900

    変更

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import SignUpForm from './SignUpForm';
 import HomePage from './HomePage';
@@ -17,17 +17,22 @@ const App: React.FC = () => {
 
   return (
     <Router>
-      <Routes>
-        <Route path="/">
-          <Route path="login" element={isLoggedIn ? <Navigate to="/home" /> : <LoginForm handleLogin={handleLogin} />} />
-          <Route path="signup" element={isLoggedIn ? <Navigate to="/home" /> : <SignUpForm handleLogin={handleLogin} />} />
-          <Route path="home" element={isLoggedIn ? <HomePage handleLogout={handleLogout} /> : <Navigate to="/login" />} />
-          <Route path="/" element={<Navigate to="/login" />} />
+      <Switch>
+        <Route exact path="/login">
+          {isLoggedIn ? <Redirect to="/home" /> : <LoginForm handleLogin={handleLogin} />}
         </Route>
-      </Routes>
+        <Route exact path="/signup">
+          {isLoggedIn ? <Redirect to="/home" /> : <SignUpForm handleLogin={handleLogin} />}
+        </Route>
+        <Route exact path="/home">
+          {isLoggedIn ? <HomePage handleLogout={handleLogout} /> : <Redirect to="/login" />}
+        </Route>
+        <Route path="/">
+          <Redirect to="/login" />
+        </Route>
+      </Switch>
     </Router>
   );
 };
 
 export default App;
-
