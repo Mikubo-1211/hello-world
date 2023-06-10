ba67bd9506ffc7a0fb80d6ac872d726589893037
Mikubo-1211 <mikubo-daichi1985@g.ecc.u-tokyo.ac.jp>
2023-06-10 13:49:52 +0900

    バグ処理

--- my-app-ts/src/SignUpForm.tsx
+++ my-app-ts/src/SignUpForm.tsx
@@ -84,6 +84,7 @@ const SignUpForm: React.FC<SignUpFormProps> = ({ handleLogin }) => {
         if (!result.ok) {
           throw Error(`Failed to create user: ${result.status}`);
         }
+        fetchUsers(email);
       } catch (err) {
         const errorMessage = (err as Error).message; // エラーオブジェクトの型を明示的に指定する
         alert(errorMessage);
@@ -93,6 +94,21 @@ const SignUpForm: React.FC<SignUpFormProps> = ({ handleLogin }) => {
       alert(errorMessage);
     }
   };
+
+  //バグ処理試験的
+  const fetchUsers = async (email: string) => {
+    try {
+      const result = await fetch(`https://hello-world-2-xyex4gyyzq-uc.a.run.app/users?user_email=${email}`, {
+        method: 'GET',
+      });
+      if (!result.ok) {
+        throw Error(`Failed to fetch user: ${result.status}`);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
 
   return (
     <div className="SignUpForm">
